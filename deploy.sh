4d419fe6f977072d1401209ec78e69df62813722
givanse <silvag777@gmail.com>
2014-07-01 16:02:49 -0500

    revisit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-# git remote add hostgator-www givanse@givan.se:www/
-
 dist=../hg-www
 
 ember build --output-path $dist
+
 cd $dist
 
-git add -u .
+git init
+
+git remote add hostgator-www givanse@givan.se:www/
+
+git add .
 
 git commit -m 'automated deploy'
 
-git push -u hostgator-www master
+git push -u -f hostgator-www master
 
 exit
 #EOF
