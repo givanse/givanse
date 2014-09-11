2403fc25ddb05699aacee552b14dd26121e9729c
givanse <silvag777@gmail.com>
2014-09-11 03:28:20 -0500

    add 'published' attribute

--- app/models/post.js
+++ app/models/post.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   title: DS.attr(),
   created: DS.attr('date'),
   updated: DS.attr('date'),
-  description: DS.attr()
+  description: DS.attr(),
+  published: DS.attr()
 });
