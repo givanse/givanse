1b1017c4d54f514d4d54533b17a894129ce3aa43
Gastón I. Silva E <silvag777@gmail.com>
2015-03-20 14:38:40 -0600

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-BROCCOLI_TACO_ENV=production broccoli-taco build dist
+set -e
 
+ghpages=../givanse-gh-pages/
+
+BROCCOLI_TACO_ENV=production broccoli-taco build dist/
+
+cp -vR dist/* $ghpages
+
+cd $ghpages
+
+git status
+
+git add -u .
+
+git commit -m 'automated deploy'
+
+git push -u origin gh-pages 
+
+exit
+#EOF
