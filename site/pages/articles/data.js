ffdc3861dd0970e37d346687ca76103d64a414d6
Gastón I. Silva E <givanse@gmail.com>
2015-04-21 14:12:28 -0500

    text edits

--- site/pages/articles/data.js
+++ site/pages/articles/data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'articles - givanse',
+  title: 'Articles :: givanse',
   description: 'Articles about software design and development, usually related to web technologies.',
   articles: true,
   articlesList: require('../../articles-list.js')
--- site/pages/data.js
+++ site/pages/data.js
@@ -1,5 +1,5 @@
 module.exports =  {
-  title: 'about - givanse',
+  title: 'Gastón I. Silva :: givanse',
   description: 'Software engineer dedicated to web technologies and open source software.',
   index: true
 };
