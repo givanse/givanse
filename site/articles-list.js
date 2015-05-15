44e2f70315cc2f7280f48a3fd8a184ab8e4726ce
Gastón I. Silva <givanse@gmail.com>
2015-05-14 21:31:11 -0500

    use keywords

--- site/articles-list.js
+++ site/articles-list.js
@@ -4,34 +4,39 @@ module.exports = [
     title: 'Broccoli.js from scratch',
     created: '19/Apr/2015',
     updated: '',
-    description: 'Build a website from scratch with Broccoli.js'
+    description: 'Build a website from scratch with Broccoli.js',
+    keywords: 'Broccoli.js'
   },
   {
     filename: 'ember-i-choose-you',
     title: 'Why I use Ember.js?',
     created: '31/Mar/2015',
     updated: '',
-    description: 'Putting aside technical aspects, what is good about Ember.js?'
+    description: 'Putting aside technical aspects, what is good about Ember.js?',
+    keywords: 'Ember.js'
   },
   {
     filename: 'mvc-past-present-and-future',
     title: 'MVC past, present and future.',
     created: '07/Aug/2014',
     updated: '',
-    description: 'Untangling MVC as popular jargon, a design pattern and a family of designs.'
+    description: 'Untangling MVC as popular jargon, a design pattern and a family of designs.',
+    keywords: 'MVC'
   },
   {
     filename: 'ember-cli-phonegap',
     title: 'Ember CLI & Phonegap',
     created: '09/Jul/2014',
     updated: '',
-    description: 'Place an Ember CLI app inside Phonegap.'
+    description: 'Place an Ember CLI app inside Phonegap.',
+    keywords: 'Ember CLI, Phonegap'
   },
   {
     filename: 'ember-cli-simple-auth-devise',
     title: 'Ember CLI & ember-simple-auth-devise',
     created: '16/Jun/2014',
     updated: '20/Oct/2014',
-    description: 'Authenticate to a Rails/Devise server from an Ember CLI app.'
+    description: 'Authenticate to a Rails/Devise server from an Ember CLI app.',
+    keywords: 'Ember CLI, Rails, Authentication'
   }
 ];
