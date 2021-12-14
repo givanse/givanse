cf5ca032d3d0d1fd5cab2c04f4ce1246425e8ff1
Gastón I. Silva <givanse@users.noreply.github.com>
2021-12-14 12:10:14 -0800

    f

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -1,4 +1,9 @@
+<style src="../../less/footer.less"></style>
 
-<a href="https://twitter.com/givanse" target="_blank" class="findme_at">
-<i class="icon-twitter"></i></a>
-<a href="https://twitter.com/givanse" target="_blank" class="findme_at">@givanse</a>
+<a href="https://twitter.com/givanse"
+   target="_blank"
+   class="findme_at"><i class="icon-twitter"></i></a>
+
+<a href="https://twitter.com/givanse"
+   target="_blank"
+   class="findme_at">@givanse</a>
