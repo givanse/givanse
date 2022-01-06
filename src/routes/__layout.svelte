96cbe9a844b5dc19c02a063c7810a28ba6ec99b6
Gastón I. Silva <givanse@users.noreply.github.com>
2022-01-06 13:51:14 -0800

    fix analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,25 +4,6 @@
 
 <script lang="ts">
 	import Header from '$lib/Header/index.svelte';
-  import { dev, browser, prerendering } from '$app/env';
-
-  /**
-	 * @type {import('@sveltejs/kit').Load}
-	 */
-	export async function load({/* page, fetch, session, context */}) {
-    if (dev || !browser || prerendering) {
-      return;
-    }
-
-    // Google Analytics
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-47511141-1', 'auto');
-    ga('send', 'pageview');
-  }
 </script>
 
 <Header />
