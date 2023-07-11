d9f09418b087a4a0d365a25de348b1cb1c56860f
Gastón Silva <givanse@users.noreply.github.com>
2023-07-10 21:55:43 -0700

    dev gate for analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,20 @@
 </svelte:head>
 
 <script lang="ts">
+  import {onMount} from "svelte";
+  import { dev, browser, prerendering } from '$app/env';
 	import Header from '$lib/Header/index.svelte';
+
+  onMount(function() {
+    if (dev || !browser || prerendering) {
+      return;
+    }
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-PLM6R0K3T7');
+  });
 </script>
 
 <Header />
